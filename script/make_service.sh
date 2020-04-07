4260070949d5b179f1e10fe960dc9f4bf16260be
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-07 16:14:35 +0000

    removed jenkins

--- script/make_service.sh
+++ script/make_service.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
-sudo cp /var/lib/jenkins/workspace/"Pipeline 1"/script/flask.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable flask.service
-sudo systemctl start flask.service
+# sudo cp /var/lib/jenkins/workspace/"Pipeline 1"/script/flask.service /etc/systemd/system
+# sudo systemctl daemon-reload
+# sudo systemctl enable flask.service
+# sudo systemctl start flask.service
 
 
