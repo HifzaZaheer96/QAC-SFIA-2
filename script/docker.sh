6cd74b204836021a5c939c88348c34f2e8152d10
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-07 12:08:16 +0000

    changed docker.sh

--- script/docker.sh
+++ script/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source --limit @/var/lib/jenkins/workspace/"Pipeline 1"/playbook.retry
+source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
 docker stack deploy --compose-file docker-compose.yml stackthemegenerator 
