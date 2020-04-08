41aa2f1a37d6a3de69c6c8da4c9cb45d16db462a
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-08 16:40:30 +0000

    working on jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,7 +6,7 @@ pipeline {
                 sh 'chmod +x ./script/*'
                 sh './script/ansible.sh'
                 sh './script/docker.sh'
-                sh './script/before_installation.sh'
+               
             }
         }
         stage('Wait for installation'){
--- script/ansible.sh
+++ script/ansible.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install python -y
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
+## install ansible with pip
+pip install --user ansible
+# check that ansible has been installed
+ansible --version
+
+
 ansible-playbook -i inventory.cfg playbook.yml --start-at-task="install packages"
