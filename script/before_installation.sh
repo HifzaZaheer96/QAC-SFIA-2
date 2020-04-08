40f76f1e8df2bb71ea0dae1b9480858feddb6a27
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-08 13:33:38 +0000

    working on jenkins

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 ## install ansible with pip
-pip install --user ansible
+pip3 install --user ansible
 # check that ansible has been installed
 ansible --version
