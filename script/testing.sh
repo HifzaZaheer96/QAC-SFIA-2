5841bc349c3d1e4fab55f6c52124a6125d4ad6a8
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-09 08:50:38 +0000

    jenkins test updated

--- script/testing.sh
+++ script/testing.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# source  ~/.bashrc
-# source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
-# coverage run -m pytest ./test/testing.py
-# coverage report
-
-source /var/lib/jenkins/.bashrc
-
-python3 -m /var/lib/jenkins/workspace/"Pipeline 1"/coverage run --source=. -m pytest test/testing.py
-python3 -m /var/lib/jenkins/workspace/"Pipeline 1"/coverage report -m
+source  ~/.bashrc
+source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
+coverage run -m pytest ./test/testing.py
+coverage report
