d409f80014592d7c0d2dccbf8e98066f200ebdcc
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-12 15:20:37 +0000

    updated s2

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -14,6 +14,6 @@ def beginning():
 
 
     
-		list = ['Red','Yellow','Blue','Grey','Orange','Pink','Black']
+	list = ['Red','Yellow','Blue','Grey','Orange','Pink','Black']
 	
 	return "Your Outfit colour is " + list[random.randrange(7)]
