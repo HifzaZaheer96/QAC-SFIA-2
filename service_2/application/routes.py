8e2dae0931f6a444109ebd7dbb50425f23c71793
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-12 15:02:18 +0000

    added txt file

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -5,7 +5,7 @@ import csv
 
 @app.route('/randomcolor', methods=['GET', 'POST'])
 def beginning():
-	number = random.randrange(0,6)
+	number = random.randrange(0,7)
     with open('/opt/service_2/application/colour.txt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
