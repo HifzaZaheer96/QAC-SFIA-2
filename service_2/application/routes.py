a2d35da9d62b1411a86df7bbd9a8309f3f5365c9
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-12 18:33:16 +0000

    services updated

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -5,16 +5,12 @@ import csv
 
 @app.route('/randomcolor', methods=['GET', 'POST'])
 def beginning():
-	number = random.randrange(0,7)
-	with open('/opt/service_2/application/colour.txt') as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-		for row in csv_reader:
-			colour = str(row[number])
-	return "Your Outfit colour is " + colour
-
-
-
-    
-		# list = ['Red','Yellow','Blue','Grey','Orange','Pink','Black']
-		
-		# return "Your Outfit colour is " + list[random.randrange(7)]
+	with open('/opt/service_2/application/colour.txt') as exampleFile: 
+		m = []
+		exampleReader = csv.reader(exampleFile)
+		colour =list(exampleReader)
+		for i in colour:
+			for j in i:
+				m.append(j)
+		colouroutfit=m[random.randrange(8)]
+	return "Your Outfit colour is " + colouroutfit
