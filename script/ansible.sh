5cfe4b15a00c671629a44c95763474ae9daa00b5
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-13 17:42:33 +0000

    ansible updated

--- script/ansible.sh
+++ script/ansible.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source  /var/lib/jenkins/.bashrc
-ansible-playbook -i inventory.cfg playbook.yml --start-at-task="install packages"
+# ansible-playbook -i inventory.cfg playbook.yml --start-at-task="install packages"
+
+ansible-playbook -i inventory.cfg playbook.yml 
